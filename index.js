7aeecb3011470c4a3070d99ff7f6138b2ae3b4a5
Doug <rdcassin@gmail.com>
2024-08-02 16:34:45 -0500

    Started on Footer

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 var clicks = 0;
 
-function nextTestimonial() {
+function switchTestimonial() {
+    
     clicks ++;
     var element = document.getElementById("testimonial__wrapper");
-    if (clicks % 2 > 0) {
+
+    if (clicks % 2 > 0 ) {
         element.classList.remove('starting-testimonial');
         element.classList += " next-testimonial";
-
     }
 
     else {
         element.classList.remove('next-testimonial');
         element.classList += " starting-testimonial";
     }
+
 }
